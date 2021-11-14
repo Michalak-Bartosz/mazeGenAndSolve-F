0297ffa09f7d977f585a40a034e215a71094596b
Michalak-Bartosz <michalak.bartoszit@gmail.com>
2021-11-14 21:50:28 +0100

    Add navbar and change logo app

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,16 @@
+export const MenuItems = [{
+        title: 'Home',
+        url: "/",
+        cName: 'navbar-links'
+    },
+    {
+        title: 'Raports',
+        url: "/raports",
+        cName: 'navbar-links'
+    },
+    {
+        title: 'Information',
+        url: "/information",
+        cName: 'navbar-links'
+    }
+]
